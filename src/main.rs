ac8322ae9d190fe8762f3b4e0e5a208b0471db5a
Danny Sauer <danny@dannysauer.com>
2020-09-06 14:20:00 -0500

    make an mqtt connection; not really working yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use rumqttc::{MqttOptions, Client, QoS};
+//use std::time::Duration;
+//use std::thread;
+//use std::io::{ErrorKind};
+
 fn main() {
+    let mut mqttoptions = MqttOptions::new("rumqtt-sync-client", "localhost", 1883);
+    mqttoptions.set_keep_alive(5);
+
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+    client.subscribe("hello/rumqtt", QoS::AtMostOnce).unwrap();
+
+    let i = 0;
+    // thread::spawn(move || for i in 0..10 {
+    client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).unwrap();
+    //    thread::sleep(Duration::from_millis(100));
+    //});
+
+
+    /*
+     * Not running:
+     * Notification = Err(Io(Os { code: 111, kind: ConnectionRefused, message: "Connection refused" }))
+     *
+     * Bad auth:
+     * Notification = Err(Io(Custom { kind: InvalidData, error: "Broker rejected connection. Reason = NotAuthorized" }))
+     *  Notification = Err(Io(Custom { kind: ConnectionReset, error: "connection reset by peer" }))
+     */
+    for (_i, notification) in connection.iter().enumerate() {
+        if notification.is_err() {
+            println!("Notification = {:?}", notification);
+            /*
+             * TODO: figure out how to get error kind :/
+             *
+            match notification.unwrap_err() {
+                //std::io::ErrorKind::ConnectionRefused => println!("refused"),
+                rumqttc::ConnectionError::Io() => println!("refused"),
+                _ => println!("kapow"),
+            }
+            */
+            break;
+           /* 
+             if notification.unwrap_err().kind() == std::io::ErrorKind::ConnectionRefused {
+                println!("refused: {:?}", notification);
+            }
+            */
+        }
+        println!("Notification = {:?}", notification);
+    }
+
     println!("Hello, world!");
 }
