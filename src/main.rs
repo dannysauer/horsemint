1d1193b8ea1a4029ff818d1659e5e9538b847687
Danny Sauer <danny@dannysauer.com>
2020-09-22 00:02:03 -0500

    Reformat according to rustfmt

--- src/main.rs
+++ src/main.rs
@@ -13,44 +13,52 @@ fn main() {
         //.merge(config::File::with_name("/etc/horsemint/horsemint")).unwrap()
         .merge(config::File::with_name("Settings")).unwrap()
         // environment vars starting with $HORSEMINT_
-        .merge(config::Environment::with_prefix("HORSEMINT")).unwrap()
-        ;
+        .merge(config::Environment::with_prefix("HORSEMINT")).unwrap();
     let mut mqttoptions = MqttOptions::new(
         settings.get_str("mqtt_channel").unwrap_or_default(),
         settings.get_str("mqtt_host").unwrap_or_default(),
-        settings.get_int("mqtt_port").unwrap_or_default() as u16
-        );
+        settings.get_int("mqtt_port").unwrap_or_default() as u16,
+    );
     mqttoptions.set_keep_alive(5);
     mqttoptions.set_credentials(
         settings.get_str("username").unwrap_or_default(),
-        settings.get_str("password").unwrap_or_default()
-        );
+        settings.get_str("password").unwrap_or_default(),
+    );
 
     let (mut client, mut connection) = Client::new(mqttoptions, 10);
     client.subscribe("hello/rumqtt", QoS::AtMostOnce).unwrap();
 
     let i = 0;
     // thread::spawn(move || for i in 0..10 {
-    client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).unwrap();
+    client
+        .publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize])
+        .unwrap();
     //    thread::sleep(Duration::from_millis(100));
     //});
 
 
     /*
      * Not running:
-     * Notification = Err(Io(Os { code: 111, kind: ConnectionRefused, message: "Connection refused" }))
+     *  Notification = Err(Io(Os {
+     *    code: 111, kind: ConnectionRefused, message: "Connection refused" }))
      *
      * Bad auth:
-     * Notification = Err(Io(Custom { kind: InvalidData, error: "Broker rejected connection. Reason = NotAuthorized" }))
-     *  Notification = Err(Io(Custom { kind: ConnectionReset, error: "connection reset by peer" }))
+     *  Notification = Err(Io(Custom {
+     *    kind: InvalidData,
+     *    error: "Broker rejected connection. Reason = NotAuthorized" }))
+     *  Notification = Err(Io(Custom {
+     *    kind: ConnectionReset, error: "connection reset by peer" }))
      *
-     *  Ok:
+     * Ok:
      *  Notification = Ok((Some(Connected), None))
      *  Notification = Ok((None, Some(Subscribe(1))))
      *  Notification = Ok((None, Some(Publish(2))))
-     *  Notification = Ok((Some(SubAck(SubAck { pkid: 1, return_codes: [Success(AtMostOnce)] })), None))
+     *  Notification = Ok((Some(SubAck(SubAck {
+     *    pkid: 1, return_codes: [Success(AtMostOnce)] })), None))
      *  Notification = Ok((Some(PubAck(PubAck { pkid: 2 })), None))
-     *  Notification = Ok((Some(Publish(Topic = hello/rumqtt, Qos = AtMostOnce, Retain = false, Pkid = 0, Payload Size = 0)), None))
+     *  Notification = Ok((Some(Publish(
+     *    Topic = hello/rumqtt, Qos = AtMostOnce,
+     *    Retain = false, Pkid = 0, Payload Size = 0)), None))
      *  Notification = Ok((None, Some(PingReq)))
      *  Notification = Ok((Some(PingResp), None))
      *  Notification = Ok((None, Some(PingReq)))
@@ -69,14 +77,15 @@ fn main() {
                 _ => println!("kapow"),
             }
             */
-           /* 
-             if notification.unwrap_err().kind() == std::io::ErrorKind::ConnectionRefused {
+            /*
+            if notification.unwrap_err().kind()
+                == std::io::ErrorKind::ConnectionRefused
+            {
                 println!("refused: {:?}", notification);
             }
             */
             break;
-        }
-        else {
+        } else {
             // success!
             let n = notification.ok().unwrap_or_default();
             println!("Notification n = {:?}", n);
